8127319aaaae0c3d1b5761d200c3f2f70617f1c2
Manish Kumar <manishmshr89@gmail.com>
2017-05-04 00:24:26 -0400

    Add files via upload

---
+++ RNNCode/loadAudio.py
@@ -0,0 +1,151 @@
+import os
+import scipy.io.wavfile as wav
+
+import numpy as np
+from python_speech_features import mfcc
+
+
+
+def load_wavfile(wavfile):
+    """
+    Read a wav file using scipy.io.wavfile
+    """
+    rate, sig = wav.read(wavfile)
+    data_name = os.path.splitext(os.path.basename(wavfile))[0]
+    return rate, sig, data_name
+
+
+
+def get_audio_and_transcript(txt_files, wav_files, n_input, n_context):
+    
+    
+    audio = []
+    audio_len = []
+    transcript = []
+    transcript_len = []
+
+    for txt_file, wav_file in zip(txt_files, wav_files):
+        # load audio and convert to features
+        audio_data = audiofile_to_input_vector(wav_file, n_input, n_context)
+        audio_data = audio_data.astype('float32')
+
+        audio.append(audio_data)
+        audio_len.append(np.int32(len(audio_data)))
+
+        # load text transcription and convert to numerical array
+        target = normalize_txt_file(txt_file)
+        target = text_to_char_array(target)
+        transcript.append(target)
+        transcript_len.append(len(target))
+
+    audio = np.asarray(audio)
+    audio_len = np.asarray(audio_len)
+    transcript = np.asarray(transcript)
+    transcript_len = np.asarray(transcript_len)
+    return audio, audio_len, transcript, transcript_len
+
+
+def audiofile_to_input_vector(audio_filename, numcep, numcontext):
+    
+   
+    fs, audio = wav.read(audio_filename)
+
+    # Get mfcc coefficients
+    inputs = mfcc(audio, samplerate=fs, numcep=numcep)
+
+    # We only keep every second feature (BiRNN stride = 2)
+    inputs = orig_inputs[::2]
+
+    
+    train_inputs = np.array([], np.float32)
+    train_inputs.resize((orig_inputs.shape[0], numcep + 2 * numcep * numcontext))
+
+    # Prepare pre-fix post fix context
+    empty_mfcc = np.array([])
+    empty_mfcc.resize((numcep))
+
+    # Prepare train_inputs with past and future contexts
+    time_slices = range(train_inputs.shape[0])
+    context_past_min = time_slices[0] + numcontext
+    context_future_max = time_slices[-1] - numcontext
+    for time_slice in time_slices:
+        ntext time slices in the past, and complete with empty
+        # mfcc features
+        need_empty_past = max(0, (context_past_min - time_slice))
+        empty_source_past = list(empty_mfcc for empty_slots in range(need_empty_past))
+        data_source_past = orig_inputs[max(0, time_slice - numcontext):time_slice]
+        assert(len(empty_source_past) + len(data_source_past) == numcontext)
+
+        # Pick up to numcontext time slices in the future, and complete with empty
+        # mfcc features
+        need_empty_future = max(0, (time_slice - context_future_max))
+        empty_source_future = list(empty_mfcc for empty_slots in range(need_empty_future))
+        data_source_future = orig_inputs[time_slice + 1:time_slice + numcontext + 1]
+        assert(len(empty_source_future) + len(data_source_future) == numcontext)
+
+        if need_empty_past:
+            past = np.concatenate((empty_source_past, data_source_past))
+        else:
+            past = data_source_past
+
+        if need_empty_future:
+            future = np.concatenate((data_source_future, empty_source_future))
+        else:
+            future = data_source_future
+
+        past = np.reshape(past, numcontext * numcep)
+        now = orig_inputs[time_slice]
+        future = np.reshape(future, numcontext * numcep)
+
+        train_inputs[time_slice] = np.concatenate((past, now, future))
+       len(train_inputs[time_slice]) == numcep + 2 * numcep * numcontext
+
+   
+    train_inputs = (train_inputs - np.mean(train_inputs)) / np.std(train_inputs)
+    return train_inputs
+
+
+def pad_sequences(sequences, maxlen=None, dtype=np.float32,
+                  padding='post', truncating='post', value=0.):
+
+    
+    lengths = np.asarray([len(s) for s in sequences], dtype=np.int64)
+
+    nb_samples = len(sequences)
+    if maxlen is None:
+        maxlen = np.max(lengths)
+
+    # take the sample shape from the first non empty sequence
+    # checking for consistency in the main loop below.
+    sample_shape = tuple()
+    for s in sequences:
+        if len(s) > 0:
+            sample_shape = np.asarray(s).shape[1:]
+            break
+
+    x = (np.ones((nb_samples, maxlen) + sample_shape) * value).astype(dtype)
+    for idx, s in enumerate(sequences):
+        if len(s) == 0:
+            continue  # empty list was found
+        if truncating == 'pre':
+            trunc = s[-maxlen:]
+        elif truncating == 'post':
+            trunc = s[:maxlen]
+        else:
+            raise ValueError('Truncating type "%s" not understood' % truncating)
+
+        # check `trunc` has expected shape
+        trunc = np.asarray(trunc, dtype=dtype)
+        if trunc.shape[1:] != sample_shape:
+            raise ValueError('Shape of sample %s of sequence at position %s is different from expected shape %s' %
+                             (trunc.shape[1:], idx, sample_shape))
+
+        if padding == 'post':
+            x[idx, :len(trunc)] = trunc
+        elif padding == 'pre':
+            x[idx, -len(trunc):] = trunc
+        else:
+            raise ValueError('Padding type "%s" not understood' % padding)
+    return x, lengths
+
+
